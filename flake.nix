a4c696ce1d2852c74999ac80db75679561f84844
Hy4ri <hiari45@gmail.com>
2026-05-25 18:10:16 +0300

    build: migrate from Go modules to Meson/C build system
    Update .gitignore to exclude build artifacts and object files.
    
    Rewrite flake.nix to replace Go/Fyne dependencies with SDL3, SDL3-image, SDL3-ttf, and libexif for the C rewrite.
    
    Add Makefile and meson.build for the new C build system.

--- flake.nix
+++ flake.nix
@@ -6,78 +6,59 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-
-        # Fyne requires OpenGL and X11/Wayland headers
-        buildInputs = with pkgs; [
-          libGL
-          mesa
-          libx11
-          libxcursor
-          libxrandr
-          libxinerama
-          libxi
-          libxxf86vm
-          wayland
-          libxkbcommon
-        ];
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        pname = "frame";
+        version = "0.9.0";
+        src = ./.;
 
         nativeBuildInputs = with pkgs; [
-          go_1_26
+          meson
+          ninja
           pkg-config
         ];
-      in {
-        packages.default = (pkgs.buildGoModule.override {go = pkgs.go_1_26;}) {
-          pname = "frame";
-          version = "0.8.0";
-          src = ./.;
 
-          vendorHash = "sha256-qtwMo29HzrB5y4oxXK+dUKioqQGk0+4BBuLpNIg3pZM=";
+        buildInputs = with pkgs; [
+          sdl3
+          sdl3-image
+          sdl3-ttf
+          libexif
+        ];
 
-          inherit buildInputs nativeBuildInputs;
-
-          env.CGO_ENABLED = "1";
-
-          # Install desktop file
-          postInstall = ''
-            mkdir -p $out/share/applications
-            cp ${./frame.desktop} $out/share/applications/frame.desktop
-          '';
-
-          meta = with pkgs.lib; {
-            description = "A minimal image viewer for Linux with vim keybindings";
-            homepage = "https://github.com/Hy4ri/frame";
-            license = licenses.mit;
-            maintainers = [];
-            platforms = platforms.linux;
-          };
+        meta = with pkgs.lib; {
+          description = "A minimal image viewer for Linux with vim keybindings";
+          homepage = "https://github.com/Hy4ri/frame";
+          license = licenses.mit;
+          maintainers = [];
+          platforms = platforms.linux;
         };
+      };
 
-        devShells.default = pkgs.mkShell {
-          inherit buildInputs;
-          nativeBuildInputs =
-            nativeBuildInputs
-            ++ (with pkgs; [
-              gopls
-              gotools
-              go-tools
-            ]);
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          meson
+          ninja
+          pkg-config
+        ];
 
-          shellHook = ''
-            export CGO_ENABLED=1
-            echo "Frame development environment loaded"
-            echo "Run 'go build' to compile, or 'go run .' to run"
-          '';
-        };
-      }
-    )
+        buildInputs = with pkgs; [
+          sdl3
+          sdl3-image
+          sdl3-ttf
+          libexif
+          clang-tools
+        ];
+
+        shellHook = ''
+          echo "Frame C development environment loaded"
+          echo "Run 'meson setup build && ninja -C build' to compile"
+          echo "Run './build/frame /path/to/image.jpg' to run"
+        '';
+      };
+    })
     // {
       # Overlay for NixOS integration
       overlays.default = final: prev: {
